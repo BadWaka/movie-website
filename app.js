160e87c22ebbe9a90177f08b71fe65a534f3d0d8
BadWaka <1456683844@qq.com>
2017-02-09 15:42:21 +0800

    前后端逻辑调通

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
+// 引入模块
 var express = require('express');
-// 端口
-var port = process.env.PORT || 3000;
 var app = express();
 
 // 设置视图根目录
@@ -8,6 +7,38 @@ app.set('views', './views');
 // 设置模板引擎node
 app.set('view engine', 'jade');
 
+// 监听端口
+var port = process.env.PORT || 3000;
 app.listen(port);
 
+// 服务成功启动日志
 console.log('movie-website start on port ' + port);
+
+// 路由
+// index page
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: '首页'
+    });
+});
+
+// detail page
+app.get('/movie/:id', function (req, res) {
+    res.render('detail', {
+        title: '详情'
+    });
+});
+
+// admin page
+app.get('/admin/movie', function (req, res) {
+    res.render('admin', {
+        title: '后台录入'
+    });
+});
+
+// list page
+app.get('/admin/list', function (req, res) {
+    res.render('list', {
+        title: '列表'
+    });
+});
